00ab5cef83e7df6767804c1396617f1fe7764677
Your Name <you@example.com>
2019-03-29 15:29:21 -0400

    Basic javascript done

--- Core2.1Tutorial/wwwroot/js/index.js
+++ Core2.1Tutorial/wwwroot/js/index.js
@@ -3,3 +3,8 @@
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    alert("Buying Item");
+});
