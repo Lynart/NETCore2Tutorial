783f4b4c2ef73038951c95a507a2dda14c0cdfe9
Your Name <you@example.com>
2019-04-02 09:45:17 -0400

    Added a view model for a view to send a controller Note there's some voodoo going on where the view auto binds without any extra @{}

--- Core2.1Tutorial/Controllers/AppController.cs
+++ Core2.1Tutorial/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core2._1Tutorial.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +23,7 @@ namespace Core2._1Tutorial.Controllers
         }
 
         [HttpPost("contact")]
-        public IActionResult Contact(object model)
+        public IActionResult Contact(ContactViewModel model)
         {
             return View();
         }
