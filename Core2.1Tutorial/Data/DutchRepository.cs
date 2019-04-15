fd5233331e09b9a62fb31b67a11c50016b258fa8
Vince Lynart <vince.lee.lien@gmail.com>
2019-04-15 14:20:46 -0400

    Example of logging

--- Core2.1Tutorial/Data/DutchRepository.cs
+++ Core2.1Tutorial/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Core2._1Tutorial.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,27 @@ namespace Core2._1Tutorial.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
+            try {
+                _logger.LogInformation("GetAllProducts was called");
             return _ctx.Products
                 .OrderBy(p => p.Title)
                 .ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to get all products: {e}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
